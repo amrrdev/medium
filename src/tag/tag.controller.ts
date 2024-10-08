f4afe9cea477c8948c21c7d1fcdc97c4c8afecf4
amrrdev <amrrdev@gmail.com>
2024-10-08 21:39:44 +0300

    feat: implement create and find operations for Tag entity

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TagService } from './tag.service';
+import { TagEntity } from './entities/tag.entity';
+import { TagDto } from './dto/tag.dto';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get()
-  findAll(): string[] {
-    return this.tagService.findAll();
+  async findAll(): Promise<{ tags: string[] }> {
+    const tags = await this.tagService.findAll();
+    return { tags: tags.map((tag) => tag.name) };
+  }
+
+  @Post()
+  async createTag(@Body() tagDto: TagDto): Promise<TagEntity> {
+    return await this.tagService.create(tagDto);
   }
 }
