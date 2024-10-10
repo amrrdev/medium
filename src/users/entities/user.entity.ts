0f288fbbad6e1fd9510800ddb2f3cad8f2bcc536
amrrdev <amrrdev@gmail.com>
2024-10-10 10:21:26 +0300

    initial user resource

---
+++ src/users/entities/user.entity.ts
@@ -0,0 +1,22 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity({ name: 'users' })
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  password: string;
+
+  @Column({ default: '' })
+  bio: string;
+
+  @Column({ default: '' })
+  image: string;
+}
