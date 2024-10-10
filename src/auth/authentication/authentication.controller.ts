daafbf902137b8d91ada67eac56025505a300c93
amrrdev <amrrdev@gmail.com>
2024-10-10 14:32:48 +0300

    Apply Auth decorator for authentication handling in AuthenticationController

--- src/auth/authentication/authentication.controller.ts
+++ src/auth/authentication/authentication.controller.ts
@@ -2,16 +2,21 @@ import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
+import { Auth } from './decorators/auth.decorator';
+import { AuthType } from './enums/auth-type.enum';
 
 @Controller('auth')
+@Auth(AuthType.BEARER)
 export class AuthenticationController {
   constructor(private readonly authenticationService: AuthenticationService) {}
 
+  @Auth(AuthType.NONE)
   @Post('sign-up')
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authenticationService.singup(signUpDto);
   }
 
+  @Auth(AuthType.NONE)
   @Post('sign-in')
   @HttpCode(HttpStatus.OK)
   signin(@Body() signInDto: SignInDto) {
