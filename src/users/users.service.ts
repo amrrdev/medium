a5288b25468355a9a19d98be914e0315b2862601
amrrdev <amrrdev@gmail.com>
2024-10-13 22:15:27 +0300

    feat(user): implement user profile management and update functionality

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { UpdateProfileDto } from './dto/update-profile.dto';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  constructor(
+    @InjectRepository(UserEntity) private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async findAll(): Promise<UserEntity | UserEntity[]> {
+    return await this.userRepository.find();
+  }
+
+  async profile(sub: number): Promise<UserEntity> {
+    return await this.userRepository.findOneBy({ id: sub });
+  }
+
+  async updateProfile(sub: number, updateProfileDto: UpdateProfileDto): Promise<UserEntity> {
+    const user = await this.userRepository.findOneBy({ id: sub });
+    const updatedUser = Object.assign(user, updateProfileDto);
+    console.log(updateProfileDto);
+    return await this.userRepository.save(updatedUser);
+  }
+}
