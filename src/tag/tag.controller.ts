b4194c94126b70ad4dd0ab71de6c8271c31a1438
amrrdev <amrrdev@gmail.com>
2024-10-13 22:16:38 +0300

    fix(tag): register findAll as async

--- src/tag/tag.controller.ts
+++ src/tag/tag.controller.ts
@@ -1,22 +1,17 @@
-import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  ParseIntPipe,
-  Post,
-} from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Post, Req } from '@nestjs/common';
 import { TagService } from './tag.service';
 import { TagEntity } from './entities/tag.entity';
 import { TagDto } from './dto/tag.dto';
+import { ActiveUser } from 'src/auth/decorators/active-user.decorator';
+import { ActiveUserData } from 'src/auth/interfaces/active-user-data.interface';
 
 @Controller('tags')
 export class TagController {
   constructor(private readonly tagService: TagService) {}
 
   @Get()
-  async findAll(): Promise<{ tags: string[] }> {
+  async findAll(@ActiveUser() activeUserData: ActiveUserData): Promise<{ tags: string[] }> {
+    console.log(activeUserData.email);
     const tags = await this.tagService.findAll();
     return { tags: tags.map((tag) => tag.name) };
   }
