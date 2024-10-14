74a936b078c5786cf471ff9c9bf4b393efa2d71e
amrrdev <amrrdev@gmail.com>
2024-10-15 00:16:39 +0300

    feat: add ArticleController with basic structure and service injection

---
+++ src/article/article.controller.ts
@@ -0,0 +1,7 @@
+import { Controller, Post } from '@nestjs/common';
+import { ArticleService } from './article.service';
+
+@Controller('articles')
+export class ArticleController {
+  constructor(private readonly articleService: ArticleService) {}
+}
