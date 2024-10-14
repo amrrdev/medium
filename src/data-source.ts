ef591cfd25d3ff4d0d78e17c3d11b6578435d287
amrrdev <amrrdev@gmail.com>
2024-10-15 00:20:26 +0300

    fix: update migration path in data source configuration to include all migration files

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,18 +2,19 @@ import { DataSource, DataSourceOptions } from 'typeorm';
 import { join } from 'path';
 import * as dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config({ path: join(__dirname, './../.env') });
 
 const dataSourceOption: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT),
-  username: process.env.USERNAME,
-  database: process.env.DATABASE,
-  password: process.env.PASSWORD,
+  username: process.env.DB_USERNAME,
+  database: process.env.DB_DATABASE,
+  password: process.env.DB_PASSWORD,
   entities: [join(__dirname, '**/*.entity{.ts,.js}')],
   synchronize: false,
-  migrations: [join(__dirname, './migrations/**/*{.ts,.js}')],
+  migrations: [join(__dirname, '**/migrations/*{.ts,.js}')],
   logging: true,
 };
+
 export const AppDataSource = new DataSource(dataSourceOption);
