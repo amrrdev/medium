e4d3facfa9a24073220c9b215e48fafafe853f4b
amrrdev <amrrdev@gmail.com>
2024-10-15 22:52:01 +0300

    refactor: mv common from auth to src

---
+++ src/common/interceptors/serialize.interceptor.ts
@@ -0,0 +1,23 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+  Type,
+} from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private readonly DTO: Type<T>) {}
+  intercept(context: ExecutionContext, next: CallHandler): Observable<T> {
+    return next.handle().pipe(
+      map((response: T) => {
+        return plainToClass(this.DTO, response, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
