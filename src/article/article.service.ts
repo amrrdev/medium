2d8587afe0849f3aada0c2ecd840956904b98958
amrrdev <amrrdev@gmail.com>
2024-10-17 22:39:04 +0300

    feat: implement ArticleService with CRUD operations
    - Added methods to create, retrieve, update, and delete articles.
    - Implemented getArticles with author relations.
    - Integrated createArticle to associate articles with authors.
    - Added checks to ensure that only article authors can update or delete articles.
    - Included a private helper method indArticleWithSpecificUser to handle article ownership validation.

--- src/article/article.service.ts
+++ src/article/article.service.ts
@@ -1,4 +1,67 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { ArticleEntity } from './entities/article.entity';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreateArticleDto } from './dto/create-article.dto';
+import { UserEntity } from '../users/entities/user.entity';
+import { UpdateArticelDto } from './dto/update-article.dto';
 
 @Injectable()
-export class ArticleService {}
+export class ArticleService {
+  constructor(
+    @InjectRepository(ArticleEntity)
+    private readonly articleRepository: Repository<ArticleEntity>,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
+
+  async getArticles() {
+    return await this.articleRepository.find({ relations: ['author'] });
+  }
+
+  async createArticle(createArticleDto: CreateArticleDto, userId: number) {
+    const author = await this.userRepository.findOneBy({ id: userId });
+    const article = this.articleRepository.create({
+      ...createArticleDto,
+      author,
+    });
+    return await this.articleRepository.save(article);
+  }
+
+  async getArticleById(articleId: number) {
+    const article = await this.articleRepository.findOne({
+      where: { id: articleId },
+      relations: ['author'],
+    });
+    if (!article) {
+      throw new NotFoundException(`There's not article with that id ${articleId}`);
+    }
+    return article;
+  }
+
+  async updateArticle(userId: number, articleId: number, updateArticelDto: UpdateArticelDto) {
+    const article = await this.findArticleWithSpecificUser(userId, articleId);
+
+    Object.assign(article, updateArticelDto);
+    return await this.articleRepository.save(article);
+  }
+
+  async deleteArticle(userId: number, articleId: number) {
+    const article = await this.findArticleWithSpecificUser(userId, articleId);
+    await this.articleRepository.delete({ id: article.id });
+  }
+
+  private async findArticleWithSpecificUser(userId: number, articleId: number) {
+    const article = await this.articleRepository.findOne({
+      where: { id: articleId, author: { id: userId } },
+      relations: ['author'],
+    });
+
+    if (!article) {
+      throw new BadRequestException(
+        `Article with ID ${articleId} does not exist or you do not have permission to edit it.`,
+      );
+    }
+    return article;
+  }
+}
