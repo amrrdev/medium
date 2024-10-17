367bd37376197e1366145e2704fc1b0f35c0d19e
amrrdev <amrrdev@gmail.com>
2024-10-17 22:38:38 +0300

    feat: create ArticleModule with TypeORM integration

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ArticleEntity } from './entities/article.entity';
+import { UserEntity } from 'src/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ArticleEntity, UserEntity])],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
