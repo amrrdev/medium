47bfb9881d3f4d3c6e61d45759f7fa5ec0b5a89e
amrrdev <amrrdev@gmail.com>
2024-10-17 22:39:56 +0300

    feat: implement Sertialize with userDto

--- src/auth/authentication/authentication.controller.ts
+++ src/auth/authentication/authentication.controller.ts
@@ -1,4 +1,4 @@
-import { Body, Controller, HttpCode, HttpStatus, Param, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Param, Post, Req } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import { SignUpDto } from './dto/sign-up.dto';
 import { SignInDto } from './dto/sign-in.dto';
@@ -6,6 +6,8 @@ import { Auth } from './decorators/auth.decorator';
 import { AuthType } from './enums/auth-type.enum';
 import { ForgetPasswordDto } from './dto/forget-password.dto';
 import { ResetPasswordDto } from './dto/reset-password.dto';
+import { Serialize } from 'src/users/decorators/serialize-user.decorator';
+import { UserDto } from 'src/users/dto/user.dto';
 
 @Controller('auth')
 @Auth(AuthType.BEARER)
@@ -14,6 +16,7 @@ export class AuthenticationController {
 
   @Auth(AuthType.NONE)
   @Post('sign-up')
+  @Serialize(UserDto)
   signUp(@Body() signUpDto: SignUpDto) {
     return this.authenticationService.singup(signUpDto);
   }
