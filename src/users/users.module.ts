f7117fc2248a6fa3ef84af8178603d8f9e777ff1
amrrdev <amrrdev@gmail.com>
2024-10-17 22:41:55 +0300

    feat: Update UserModule imports to include ArticleEntity and ArticleModule

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,11 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserEntity } from './entities/user.entity';
+import { ArticleEntity } from 'src/article/entities/article.entity';
+import { ArticleModule } from 'src/article/article.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UserEntity])],
+  imports: [TypeOrmModule.forFeature([UserEntity, ArticleEntity]), ArticleModule],
   controllers: [UsersController],
   providers: [UsersService],
 })
