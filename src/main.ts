99196e235083b2d035d8ecdf415a3733c91896aa
amrrdev <amrrdev@gmail.com>
2024-10-22 23:43:58 +0300

    feat(docs): configure Swagger for Medium-Like Blogging Platform API

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix('api/v1');
   app.useGlobalPipes(new ValidationPipe());
+
+  const options = new DocumentBuilder()
+    .setTitle('Medium-Like Blogging Platform API')
+    .setDescription(
+      `This API powers a blogging platform inspired by Medium, allowing users to create, edit, delete,
+       and browse articles. It supports user authentication,
+       article tagging, and enables users to engage with content through comments.`,
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('api/v1/docs', app, document);
   await app.listen(3000);
 }
 bootstrap();
