62d987186c9ca5a3017b2750d5dcb4b016605574
amrrdev <amrrdev@gmail.com>
2024-10-22 23:37:05 +0300

    chore(database): register Article, User, and Comment entities in TypeORM module

--- src/article/article.module.ts
+++ src/article/article.module.ts
@@ -3,10 +3,15 @@ import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ArticleEntity } from './entities/article.entity';
-import { UserEntity } from 'src/users/entities/user.entity';
+import { UserEntity } from '../users/entities/user.entity';
+import { CommentEntity } from '../comment/entities/comment.entity';
+import { CommentModule } from '../comment/comment.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([ArticleEntity, UserEntity])],
+  imports: [
+    TypeOrmModule.forFeature([ArticleEntity, UserEntity, CommentEntity]),
+    CommentModule,
+  ],
   controllers: [ArticleController],
   providers: [ArticleService],
 })
